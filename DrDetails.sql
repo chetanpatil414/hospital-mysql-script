8b569b53f0073497d160916527b8472c00a08f96
chetanpatil414 <chetanpatil414@gmail.com>
2023-12-17 18:27:55 +0530

    initial change for DrDetails

---
+++ DrDetails.sql
@@ -0,0 +1,19 @@
+use hospital
+
+create table Patient(
+id int PRIMARY KEY AUTO_INCREMENT,
+first_name varchar(20),
+last_name varchar(30),
+email varchar(30),
+phone_no varchar(30),
+referral varchar(30),
+decease varchar (60),
+patient_description varchar(225),
+dr_Assigned varchar(30)
+)
+
+select * from Patient
+
+select * from doctors_data
+
+drop table patient
